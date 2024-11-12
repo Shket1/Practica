3be2c671443a399ab5372fd10854f90958796449
Shket1 <ma4e94@gmail.com>
2024-11-12 16:51:08 +0200

    Добавлена ммини реализация ArrayList и LinkedList

---
+++ src/practic/collections/handMadeArrayList/Main.java
@@ -0,0 +1,26 @@
+package practic.collections.handMadeArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        HandMadeArrayList<Integer> list = new HandMadeArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+        list.add(11);
+        list.add(12);
+        list.add(13);
+        list.add(14);
+        list.add(15);
+        list.add(16);
+        list.add(17);
+        System.out.println(list.size());
+    }
+}
